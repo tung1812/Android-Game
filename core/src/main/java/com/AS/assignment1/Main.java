6b7379ed9b654adedd9ec38bbf030c5fed18f0e4
a3145117 <phagy004@mymail.unisa.edu.au>
2026-06-02 17:34:17 +0930

    Adding asset to main menu function, add credit to main menu

--- core/src/main/java/com/AS/assignment1/Main.java
+++ core/src/main/java/com/AS/assignment1/Main.java
@@ -4,27 +4,44 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Main extends ApplicationAdapter {
 
     public enum GameState {
         MENU,
         HELP,
-        PLAYING
+        PLAYING,
+        CREDIT
     }
 
     GameState gameState = GameState.MENU;
 
     SpriteBatch batch;
 
-    BitmapFont titleFont;
-    BitmapFont menuFont;
-    BitmapFont smallFont;
+    Texture backgroundTexture;
+    Texture startButtonTexture;
+    Texture optionButtonTexture;
+    Texture creditButtonTexture;
+    Texture quitButtonTexture;
+    Texture menuButtonTexture;
+    Texture darkBoxTexture;
 
+    Rectangle startButton;
+    Rectangle optionButton;
+    Rectangle creditButton;
+    Rectangle quitButton;
+    Rectangle menuButton;
+
+    BitmapFont titleFont;
+    BitmapFont smallFont;
     GlyphLayout layout;
 
     float screenWidth;
@@ -33,24 +50,51 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
-
         Gdx.input.setCatchKey(Input.Keys.BACK, true);
 
         batch = new SpriteBatch();
 
-        titleFont = new BitmapFont();
-        titleFont.getData().setScale(5);
+        backgroundTexture = new Texture("background.jpg");
+        startButtonTexture = new Texture("Menu/start.png");
+        optionButtonTexture = new Texture("Menu/option.png");
+        creditButtonTexture = new Texture("Menu/credit.png");
+        quitButtonTexture = new Texture("Menu/quit.png");
+        menuButtonTexture = new Texture("icon/home button.png");
 
-        menuFont = new BitmapFont();
-        menuFont.getData().setScale(3);
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(0, 0, 0, 0.78f);
+        pixmap.fill();
+        darkBoxTexture = new Texture(pixmap);
+        pixmap.dispose();
+
+        titleFont = new BitmapFont();
+        titleFont.getData().setScale(5.5f);
+        titleFont.setColor(Color.WHITE);
 
         smallFont = new BitmapFont();
-        smallFont.getData().setScale(2);
+        smallFont.getData().setScale(2.6f);
+        smallFont.setColor(Color.WHITE);
 
         layout = new GlyphLayout();
 
         screenWidth = Gdx.graphics.getWidth();
         screenHeight = Gdx.graphics.getHeight();
+
+        float buttonWidth = screenWidth * 0.28f;
+        float buttonHeight = screenHeight * 0.11f;
+        float buttonX = (screenWidth - buttonWidth) / 2;
+
+        startButton = new Rectangle(buttonX, screenHeight * 0.58f, buttonWidth, buttonHeight);
+        optionButton = new Rectangle(buttonX, screenHeight * 0.44f, buttonWidth, buttonHeight);
+        creditButton = new Rectangle(buttonX, screenHeight * 0.30f, buttonWidth, buttonHeight);
+        quitButton = new Rectangle(buttonX, screenHeight * 0.16f, buttonWidth, buttonHeight);
+
+        menuButton = new Rectangle(
+            screenWidth * 0.04f,
+            screenHeight * 0.82f,
+            screenHeight * 0.12f,
+            screenHeight * 0.12f
+        );
     }
 
     private void update() {
@@ -66,26 +110,31 @@ public class Main extends ApplicationAdapter {
             case PLAYING:
                 updateGame();
                 break;
+
+            case CREDIT:
+                updateCredit();
+                break;
         }
     }
 
     private void updateMenu() {
-        float startY = screenHeight * 0.62f;
-        float helpY = screenHeight * 0.48f;
-        float exitY = screenHeight * 0.34f;
-
         if (Gdx.input.justTouched()) {
+            float touchX = Gdx.input.getX();
             float touchY = screenHeight - Gdx.input.getY();
 
-            if (touchY <= startY + 40 && touchY >= startY - 60) {
+            if (startButton.contains(touchX, touchY)) {
                 gameState = GameState.PLAYING;
             }
 
-            if (touchY <= helpY + 40 && touchY >= helpY - 60) {
+            if (optionButton.contains(touchX, touchY)) {
                 gameState = GameState.HELP;
             }
 
-            if (touchY <= exitY + 40 && touchY >= exitY - 60) {
+            if (creditButton.contains(touchX, touchY)) {
+                gameState = GameState.CREDIT;
+            }
+
+            if (quitButton.contains(touchX, touchY)) {
                 Gdx.app.exit();
             }
         }
@@ -98,6 +147,10 @@ public class Main extends ApplicationAdapter {
             gameState = GameState.HELP;
         }
 
+        if (Gdx.input.isKeyJustPressed(Input.Keys.C)) {
+            gameState = GameState.CREDIT;
+        }
+
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE) ||
             Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
             Gdx.app.exit();
@@ -105,30 +158,43 @@ public class Main extends ApplicationAdapter {
     }
 
     private void updateHelp() {
-        float backY = screenHeight * 0.12f;
-
         if (Gdx.input.justTouched()) {
+            float touchX = Gdx.input.getX();
             float touchY = screenHeight - Gdx.input.getY();
 
-            if (touchY <= backY + 40 && touchY >= backY - 60) {
+            if (menuButton.contains(touchX, touchY)) {
                 gameState = GameState.MENU;
             }
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE) ||
-            Gdx.input.isKeyJustPressed(Input.Keys.BACK) ||
-            Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+            Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
+            gameState = GameState.MENU;
+        }
+    }
+
+    private void updateCredit() {
+        if (Gdx.input.justTouched()) {
+            float touchX = Gdx.input.getX();
+            float touchY = screenHeight - Gdx.input.getY();
+
+            if (menuButton.contains(touchX, touchY)) {
+                gameState = GameState.MENU;
+            }
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE) ||
+            Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
             gameState = GameState.MENU;
         }
     }
 
     private void updateGame() {
-        float menuY = screenHeight * 0.15f;
-
         if (Gdx.input.justTouched()) {
+            float touchX = Gdx.input.getX();
             float touchY = screenHeight - Gdx.input.getY();
 
-            if (touchY <= menuY + 40 && touchY >= menuY - 60) {
+            if (menuButton.contains(touchX, touchY)) {
                 gameState = GameState.MENU;
             }
         }
@@ -148,6 +214,8 @@ public class Main extends ApplicationAdapter {
 
         batch.begin();
 
+        batch.draw(backgroundTexture, 0, 0, screenWidth, screenHeight);
+
         switch (gameState) {
             case MENU:
                 drawMenu();
@@ -160,49 +228,87 @@ public class Main extends ApplicationAdapter {
             case PLAYING:
                 drawGame();
                 break;
+
+            case CREDIT:
+                drawCredit();
+                break;
         }
 
         batch.end();
     }
 
     private void drawMenu() {
-        drawCenteredText(titleFont, "Assignment 2 adventure rpg game", screenHeight * 0.82f);
+        drawBoldTextWithBox(titleFont, "Assignment 2 RPG Games", screenHeight * 0.86f, 45, 22);
 
-        drawCenteredText(menuFont, "START", screenHeight * 0.62f);
-        drawCenteredText(menuFont, "HELP", screenHeight * 0.48f);
-        drawCenteredText(menuFont, "EXIT", screenHeight * 0.34f);
-
-        drawCenteredText(smallFont, "Touch START to start the game.", screenHeight * 0.18f);
-        drawCenteredText(smallFont, "Need help? Right under.", screenHeight * 0.10f);
+        batch.draw(startButtonTexture, startButton.x, startButton.y, startButton.width, startButton.height);
+        batch.draw(optionButtonTexture, optionButton.x, optionButton.y, optionButton.width, optionButton.height);
+        batch.draw(creditButtonTexture, creditButton.x, creditButton.y, creditButton.width, creditButton.height);
+        batch.draw(quitButtonTexture, quitButton.x, quitButton.y, quitButton.width, quitButton.height);
     }
 
     private void drawHelp() {
-        drawCenteredText(titleFont, "HELP", screenHeight * 0.82f);
+        batch.draw(menuButtonTexture, menuButton.x, menuButton.y, menuButton.width, menuButton.height);
 
-        drawCenteredText(smallFont, "Touch buttons to control the player", screenHeight * 0.54f);
-        drawCenteredText(smallFont, "Attack enemies and reach the goal point", screenHeight * 0.44f);
-        drawCenteredText(smallFont, "If your HP reaches 0, the player dies", screenHeight * 0.34f);
+        drawBoldTextWithBox(titleFont, "Option", screenHeight * 0.78f, 45, 22);
 
-        drawCenteredText(menuFont, "BACK", screenHeight * 0.12f);
+        drawBoldTextWithBox(smallFont, "Touch buttons to control the player", screenHeight * 0.60f, 35, 18);
+        drawBoldTextWithBox(smallFont, "Attack enemies and reach the goal", screenHeight * 0.50f, 35, 18);
+        drawBoldTextWithBox(smallFont, "If your HP reaches 0, the player dies", screenHeight * 0.40f, 35, 18);
+        drawBoldTextWithBox(smallFont, "Tap the home button to go back", screenHeight * 0.30f, 35, 18);
+    }
+
+    private void drawCredit() {
+        batch.draw(menuButtonTexture, menuButton.x, menuButton.y, menuButton.width, menuButton.height);
+
+        drawBoldTextWithBox(titleFont, "Credit", screenHeight * 0.78f, 45, 22);
+
+        drawBoldTextWithBox(smallFont, "Game created by", screenHeight * 0.60f, 35, 18);
+        drawBoldTextWithBox(smallFont, "Gia Minh Pham and Son Tung Nguyen", screenHeight * 0.50f, 35, 18);
+        drawBoldTextWithBox(smallFont, "2D RPG Adventure Game", screenHeight * 0.40f, 35, 18);
+        drawBoldTextWithBox(smallFont, "Tap the home button to go back", screenHeight * 0.30f, 35, 18);
     }
 
     private void drawGame() {
-        drawCenteredText(menuFont, "MENU", screenHeight * 0.15f);
+        batch.draw(menuButtonTexture, menuButton.x, menuButton.y, menuButton.width, menuButton.height);
+
+        drawBoldTextWithBox(titleFont, "LEVEL 1", screenHeight * 0.70f, 45, 22);
     }
 
-    private void drawCenteredText(BitmapFont font, String text, float y) {
+    private void drawBoldTextWithBox(BitmapFont font, String text, float y, float paddingX, float paddingY) {
         layout.setText(font, text);
 
-        float x = (screenWidth - layout.width) / 2;
+        float textX = (screenWidth - layout.width) / 2;
+        float boxX = textX - paddingX;
+        float boxY = y - layout.height - paddingY;
+        float boxWidth = layout.width + paddingX * 2;
+        float boxHeight = layout.height + paddingY * 2;
 
-        font.draw(batch, text, x, y);
+        batch.draw(darkBoxTexture, boxX, boxY, boxWidth, boxHeight);
+
+        font.setColor(Color.BLACK);
+        font.draw(batch, text, textX + 4, y - 4);
+
+        font.setColor(Color.WHITE);
+        font.draw(batch, text, textX, y);
+        font.draw(batch, text, textX + 1, y);
+        font.draw(batch, text, textX - 1, y);
+        font.draw(batch, text, textX, y + 1);
+        font.draw(batch, text, textX, y - 1);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+
+        backgroundTexture.dispose();
+        startButtonTexture.dispose();
+        optionButtonTexture.dispose();
+        creditButtonTexture.dispose();
+        quitButtonTexture.dispose();
+        menuButtonTexture.dispose();
+        darkBoxTexture.dispose();
+
         titleFont.dispose();
-        menuFont.dispose();
         smallFont.dispose();
     }
 }
